782b2ff3b08906e3a590365c9924a35531034f51
denymulya <denymulya93@gmail.com>
2019-02-19 23:52:29 +0800

    Inisialisasi File Project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
+
+@app.route('/deny')
+def deny():
+	return 'oke'
+
+if __name__ == "__main__":
+	app.run(debug=True, port=5000)
